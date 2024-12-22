33ea12efc1c120b153d8921932e36b10ac2b5a58
Phunyeletso <104882780+Phunyeletso@users.noreply.github.com>
2024-12-22 10:44:12 +0200

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -44,24 +44,8 @@ export const navigation = [
   },
   {
     id: "1",
-    title: "Pricing",
+    title: "The Movement",
     url: "#pricing",
-  },
-  {
-    id: "2",
-    title: "How to use",
-    url: "#how-to-use",
-  },
-  {
-    id: "3",
-    title: "Roadmap",
-    url: "#roadmap",
-  },
-  {
-    id: "4",
-    title: "New account",
-    url: "#signup",
-    onlyMobile: true,
   },
   {
     id: "5",
