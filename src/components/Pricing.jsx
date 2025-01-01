4f15176ce783f797d67e511fcd9326fb1481b801
Phunyeletso <104882780+Phunyeletso@users.noreply.github.com>
2025-01-01 21:01:44 +0100

    Update Pricing.jsx

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -52,8 +52,8 @@ const Pricing = () => {
             className="inline-flex items-center justify-center gap-2 text-white bg-blue-600 hover:bg-blue-700 px-6 py-3 rounded-lg font-semibold text-lg w-auto max-w-xs mx-auto"
           >
             <div className="flex items-center justify-center">
-              <span className="material-icons">download</span>
-              <span className="ml-2">newFrequency App for Android</span>
+              <span className="material-icons"></span>
+              <span className="ml-2">Download newFrequency App for Android</span>
             </div>
           </a>
         </div>
